e198b121891157e7829f7d6e638f8d725d11d5d0
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-24 03:36:58 +0530

    data model created and updated and made a class AppleProducts and defined its properties

---
+++ MVC(conceptUnderstanding)/Model/AppleProduct.swift
@@ -0,0 +1,22 @@
+//
+//  AppleProduct.swift
+//  MVC(conceptUnderstanding)
+//
+//  Created by student on 24/04/24.
+//
+
+import Foundation
+
+class AppleProduct{
+    var name : String
+    var color : String
+    var price : Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
+
+
