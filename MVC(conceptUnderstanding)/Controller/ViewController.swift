3e91313423c852facb3b453fe32d2a61668811a7
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-24 04:18:07 +0530

    Configured the view Controller . Created the outlet labels . Created the instance of Apple Product and passed the values . Then changed the text labels accordingly as required.

--- MVC(conceptUnderstanding)/Controller/ViewController.swift
+++ MVC(conceptUnderstanding)/Controller/ViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var iPhoneNameLabel: UILabel!
+    @IBOutlet weak var iiPhoneColorLabel: UILabel!
+    
+    @IBOutlet weak var iPhonePriceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let appleProduct = AppleProduct(name: "iPhone X", color: "Space Grey", price: 999.99)
+        
+        iPhoneNameLabel.text = appleProduct.name
+        iiPhoneColorLabel.text = "in \(appleProduct.color)"
+        iPhonePriceLabel.text = "$\(appleProduct.price)"
     }
+    
+    
 
 
 }
